64d217f97fadadef63fe66e48ecfb777d2118288
sverrsr <sverre.romoeren+github@hotmail.com>
2026-01-19 13:12:59 +0100

    Refactor and update DNS configuration and scripts structure
    Removed obsolete DNS configuration files and added new configuration files for re1000 cases. Moved figure scripts, postprocessing, and preprocessing scripts into dedicated subdirectories for better organization. Deleted an unused ASV script and updated file paths to reflect the new structure.

---
+++ configurations/+cfg/dns_re1000_we20.m
@@ -0,0 +1,16 @@
+function [outputArg1,outputArg2] = dns_re1000_we20(inputArg1,inputArg2)
+%DNS_RE1000_WE20 Summary of this function goes here
+%   Detailed explanation goes here
+arguments (Input)
+    inputArg1
+    inputArg2
+end
+
+arguments (Output)
+    outputArg1
+    outputArg2
+end
+
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
