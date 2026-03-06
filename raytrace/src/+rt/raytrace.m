bad2f16c7980ba36892554eff297666363214f69
sverrsr <sverre.romoeren+github@hotmail.com>
2026-03-06 15:25:58 +0100

    Update raytrace.m

--- raytrace/src/+rt/raytrace.m
+++ raytrace/src/+rt/raytrace.m
@@ -33,7 +33,7 @@ for d = distances
     tStart = tic;
     barLength = 30;
 
-    for k = 11786:Nt
+    for k = 1:Nt
         S = load(fullfile(surfElevDir, snapshotFiles(k).name));
 
         if isfield(S, 'surfElev')
